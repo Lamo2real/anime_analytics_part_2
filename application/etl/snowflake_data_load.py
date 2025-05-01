4e576076985166f931bf5c324e4e2c354ad71612
itachi <lamochi02@gmail.com>
2025-05-01 22:33:43 +0000

    bulk load to snowflake with write_pandas module

--- application/etl/snowflake_data_load.py
+++ application/etl/snowflake_data_load.py
@@ -1,7 +1,45 @@
 
 import pandas as pd
-
+import snowflake.connector
+from snowflake.connector.error import DatabaseError, InterfaceError
+from get_secrets import get_secrets_manager_values
+from snoflake.connector.pandas_tools import write_pandas as wp
 
 def load_data_to_snoflake(genre_df, studio_df, anime_df, bridge_df):
-    """load to snowflake's each table in bulk"""
+    """
+    use aws secrets manager secrets, fecth the secrets
+    and use them as credentials when 
+    COPYing csv into snowflake's database correlating table in bulk.
+    """
     
+    try:
+        secrets = get_secrets_manager_values()
+        
+        if None in secrets.values():
+            raise KeyError
+        snowpy_con = snowflake.connector.connect(
+                user      = secrets['USER'],
+                password  = secrets['PASSWORD'],
+                account   = secrets['ACCOUNT'],
+                role      = secrets['ROLE'],
+                warehouse = 'anime_analytics_wh',
+                database  = 'anime_analytics_db',
+                schema    = 'analytics'
+                )
+        
+        all_dataframes = [
+                (genre_df, 'dim_genre'),
+                (studio_df, 'dim_studio'),
+                (anime_df, 'fact_anime'),
+                (bridge_df, 'bridge_genre_anime')
+        ]
+
+        for df, db_table in all_dataframes:
+            wp(snowpy_con, df, table_name=db_table)
+        
+    except DatabaseError as dbe:
+        raise dbe
+    except InterfaceError as ie:
+        raise ie 
+    except Exception as e:
+        raise e
