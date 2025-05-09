f6021b255cbe9edf66f18c6d025a46b08b9b4b84
Lamo Kouravand <lamochi02@gmail.com>
2025-05-09 14:19:32 +0200

    changed the job to a glueetl kind

--- infrastructure/glue/glue_job.tf
+++ infrastructure/glue/glue_job.tf
@@ -3,16 +3,15 @@
 resource "aws_glue_job" "et_job" {
     name = "glue-etl-anime-analytics-ingestion"
     role_arn = var.glue_role_arn
-    max_capacity = 0.1
+    max_capacity = 0.5
 
     command {
-      name = "pythonshell"
+      name = "glueetl"
       script_location = "s3://${var.data_lake_bucket_name}/etl/etl_script.py"
-      python_version = "3"
     }
     
     default_arguments = {
-        # "--job-language" = "python"
+        "--job-language" = "python",
         "--additional-python-modules" = "snowflake-connector-python==3.14.1,pandas==2.2.3,boto3==1.38.2",
         "--extra-py-files" = "s3://${var.data_lake_bucket_name}/etl/etl_package.zip",
         "--enable-job-insights" = "true"
